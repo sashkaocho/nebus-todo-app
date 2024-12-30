0284fccfc097723085a8fd619afaa068f4525285
sashkaochigava <sashkaochi@gmail.com>
2024-12-30 14:28:41 +0400

    feat: created BaseAlertDialog.vue

---
+++ components/base/BaseAlertDialog.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog"
+import type { ITask } from "~/types/models/task.model"
+
+defineProps<{
+  task: ITask
+}>()
+
+const emit = defineEmits(["action"])
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger as-child @click.stop>
+      <slot />
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogDescription>
+          This action cannot be undone. This will permanently delete
+          <span class="font-bold">{{ task.title }}</span>
+          task
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction @click.left="emit('action')"
+          >Delete
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
